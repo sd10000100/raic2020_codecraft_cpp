9841b5224a3c34918992c048cb6badb8fbbee98f
sd <deryuzhovas@gmail.com>
2021-01-05 00:42:12 +0300

    добавление класса армии

---
+++ Army.hpp
@@ -0,0 +1,61 @@
+#ifndef _ARMY_HPP_
+#define _ARMY_HPP_
+
+#include "model/Model.hpp"
+#include "Squard.hpp"
+#include <map>
+
+using namespace std;
+
+struct Army {
+public:
+    map<int,Squard> squards;
+    Army(){};
+    
+    // void addAndUpdate(int presentTick, Entity entity){
+    //     if(entity.entityType==RANGED_BASE)
+    //         _addAndUpdate(presentTick, entity, rangeBuilds);
+    //     if(entity.entityType==MELEE_BASE)
+    //         _addAndUpdate(presentTick, entity, meleeBuilds);
+    //     if(entity.entityType==HOUSE)
+    //         _addAndUpdate(presentTick, entity, houseBuilds);
+    //     if(entity.entityType==TURRET)
+    //         _addAndUpdate(presentTick, entity, turretBuilds);
+    //     if(entity.entityType==BUILDER_BASE)
+    //         _addAndUpdate(presentTick, entity, builderBuilds);
+    // }
+
+
+    // void removeRuin(int presentTick){
+    //     _removeRuin(presentTick, rangeBuilds);
+    //     _removeRuin(presentTick, meleeBuilds);
+    //     _removeRuin(presentTick, houseBuilds);
+    //     _removeRuin(presentTick, turretBuilds);
+    //     _removeRuin(presentTick, builderBuilds);
+    // }
+
+private:
+    // void _removeRuin(int presentTick, map<int,Entity> items){
+    //     std::map<int, Entity>::iterator sBIter = items.begin();
+    //     while (sBIter != items.end()) {
+    //         if (sBIter->second.tick!=presentTick)
+    //             sBIter = items.erase(sBIter); 
+    //         else
+    //             ++sBIter;
+    //     }
+    // }
+
+
+    // void _addAndUpdate(int presentTick, Entity entity, map<int,Entity> &items){
+    //     if(!items.count(entity.id))
+    //             {
+    //                 items[entity.id] = Entity(entity);
+    //                 items[entity.id].tick = presentTick;
+    //             }
+    //             else {
+    //                 items[entity.id].tick = presentTick;
+    //     }
+    // }
+};
+
+#endif
